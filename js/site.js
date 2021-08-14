1fe8d124b9e8df325f567d4d3256f1bdbce18908
AdmerDev <sjkrantz@aol.com>
2021-08-14 17:54:02 -0500

    FinishedSite

--- js/site.js
+++ js/site.js
@@ -1,6 +1,7 @@
 function getValues() 
 {
-    document.getElementById("amortizationTable").classList.add("invisible");
+    //Ensure the Amortization Table is hidden.
+    document.getElementById("results").classList.add("invisible");
 
     //Get the user values from the page.
     let loanAmount = document.getElementById("loanAmount").value;
@@ -8,46 +9,55 @@ function getValues()
     let rate = document.getElementById("rate").value;
     
     //Parse for numbers.
-    loanAmount = parseInt(loanAmount);
-    term = parseInt(term);
-    rate = parseInt(rate);
+    loanAmount = parseFloat(loanAmount);
+    term = parseFloat(term);
+    rate = parseFloat(rate);
 
-    let numbers = generateNumbers(loanAmount,term,rate);
+    let numbers = generateNumbers(loanAmount, term, rate);
 
     displayNumbers(numbers);
 
 }
 
 //Create the Amortization Table data.
-function generateNumbers(loanAmount,term,rate){
+function generateNumbers(loanAmount, term, rate){
 
     let numbers = [];
 
-    for (let index = 1; index <= term; index++) {
-        let payment = Number((loanAmount*(rate/1200)/(1-(1+rate/1200)**(-term))).toFixed(2));
-        let interest = Number((loanAmount * rate/1200).toFixed(2));
-        let principalPayment = Number((payment - interest).toFixed(2));
-        let remainingBalance = Number((loanAmount - principalPayment).toFixed(2));
-        let interestTotal = Number(0);
-        interestTotal += interest[index];
+    //Calculate the amounts.
+    let payment = (loanAmount*(rate/1200)/(1-(1+rate/1200)**(-term))).toFixed(2);
+    let interest = loanAmount * rate/1200;
+    let principalPayment = payment - interest;
+    let remainingBalance = loanAmount;
+    let interestTotal = 0;
 
-        numbers.push(`<tr><td>${index}</td><td>${payment}</td><td>${principalPayment}</td><td>${interest}</td><td>${interestTotal}</td><td>${remainingBalance}</td></tr>`);
+    //Create the table and fill in the data.
+    for (let index = 1; index <= term; index++) {
+        interest = remainingBalance * rate/1200;
+        principalPayment = payment - interest;
+        interestTotal += interest;
+        remainingBalance -= principalPayment;
+
+        numbers += (`<tr><td>${index}</td><td>${payment}</td><td>${principalPayment.toFixed(2)}</td><td>${interest.toFixed(2)}</td><td>${interestTotal.toFixed(2)}</td><td>${remainingBalance.toFixed(2)}</td></tr>`);
+        
     }
 
-    return numbers;
+    let returnObj = {numbers, payment, loanAmount, interestTotal};
+
+    return returnObj;
 
 }
 
-
 //Display the Amortization Table data.
-function displayNumbers(numbers){
+function displayNumbers(returnObj){
 
-    let templateRows = "";
+    let options = { style: 'currency', currency: 'USD' };
 
-    templateRows = numbers;
-
-    document.getElementById("amortizationTable").classList.remove("invisible");
-
-    document.getElementById("results").innerHTML = templateRows
+    document.getElementById("results").classList.remove("invisible");
+    document.getElementById("results").innerHTML = returnObj.numbers
+    document.getElementById("monthlyPayment").innerHTML = new Intl.NumberFormat('en-US', options).format(returnObj.payment);
+    document.getElementById("totalPrincipal").innerHTML = new Intl.NumberFormat('en-US', options).format(returnObj.loanAmount);
+    document.getElementById("totalInterest").innerHTML = new Intl.NumberFormat('en-US', options).format(returnObj.interestTotal);
+    document.getElementById("totalCost").innerHTML = new Intl.NumberFormat('en-US', options).format(returnObj.loanAmount + returnObj.interestTotal);
 
 }
